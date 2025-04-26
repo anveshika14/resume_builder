6d665ad4c2fa9e638427ec0a5b844b76aea38925
anveshika14 <anveshikashukla144@gmail.com>
2025-04-26 17:36:33 +0530

    Button added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { Button } from './components/ui/button'
 
 
 
@@ -9,7 +10,8 @@ function App() {
 
   return (
     <>
-    
+      Click here to check.
+      <Button>Click me</Button>
     </>
   )
 }
