f80a1d6478624db7e0dd2c3cc894bb17efd08a30
anveshika14 <anveshikashukla144@gmail.com>
2025-04-26 17:31:16 +0530

    update files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Loginpage from './components/mycomponents/Loginpage'
+
 
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-     <Loginpage />
+    
     </>
   )
 }
